d5badceff6af614dc5f055adbe32f67ca48b0217
apospelov13 <small99@hotbox.ru>
2017-11-02 13:53:49 +0300

    v2

--- js/common.js
+++ js/common.js
@@ -29,3 +29,14 @@ $(window).scroll(function() {
     }
   });
 });
+$(function(){
+  $("#nav .dropdown").hover(
+    function() {
+      $('#products-menu.dropdown-menu', this).stop( true, true ).fadeIn("fast");
+      $(this).toggleClass('open');
+    },
+    function() {
+      $('#products-menu.dropdown-menu', this).stop( true, true ).fadeOut("fast");
+      $(this).toggleClass('open');
+    });
+});
